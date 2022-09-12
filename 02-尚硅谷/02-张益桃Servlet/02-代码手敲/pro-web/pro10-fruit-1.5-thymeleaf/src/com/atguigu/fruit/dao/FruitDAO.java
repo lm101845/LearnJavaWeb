04c2bb9fab6fcf155e9b748c3fb5b5d2ec8bf2b9
lm101845 <1018457683@qq.com>
2022-09-12 16:36:26 +0800

    张益桃第31-35集看完

---
+++ 02-尚硅谷/02-张益桃Servlet/02-代码手敲/pro-web/pro10-fruit-1.5-thymeleaf/src/com/atguigu/fruit/dao/FruitDAO.java
@@ -0,0 +1,20 @@
+package com.atguigu.fruit.dao;
+
+import com.atguigu.fruit.pojo.Fruit;
+
+import java.util.List;
+
+/**
+ * @Author liming
+ * @Date 2022/9/7 11:11
+ **/
+public interface FruitDAO {
+    //获取所有的库存列表信息
+    List<Fruit> getFruitList();
+
+    //根据fid获取特定的水果库存信息
+    Fruit getFruitByFid(Integer fid);
+
+    //修改指定的库存记录
+    void updateFruit(Fruit fruit);
+}
